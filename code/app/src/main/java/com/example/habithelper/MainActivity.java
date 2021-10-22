c6d5383e00c1d11745a0e68e09581eb4af331ae4
= <skousbol@ualberta.ca>
2021-10-22 16:45:56 -0600

    More tweaks to login page

--- code/app/src/main/java/com/example/habithelper/MainActivity.java
+++ code/app/src/main/java/com/example/habithelper/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.habithelper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -13,9 +14,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        setContentView(R.layout.activity_login);
 
         db = FirebaseFirestore.getInstance();
-        final CollectionReference collectionReference = db.collection("Cities");
+        final CollectionReference userCollectionReference = db.collection("Users");
+    }
+
+    public void onLoginSignUpClick(View view){
+
     }
 }
