9976e5eb9b3f9d701b6173e2967ed33fec788a9d
Aqil <91294196+v6k@users.noreply.github.com>
2021-10-25 23:14:01 -0600

    Added create habit

--- homescreen/app/src/main/java/com/example/homescreen/MainActivity.java
+++ homescreen/app/src/main/java/com/example/homescreen/MainActivity.java
@@ -7,18 +7,31 @@ import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.material.badge.BadgeDrawable;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    public FloatingActionButton floatingActionButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        floatingActionButton = findViewById(R.id.fab);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CreateHabit.class);
+                startActivity(intent);
+            }
+        });
 
 
 
