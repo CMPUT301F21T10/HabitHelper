8121080fb69427bcefefebc4de85cc51f7b0a86b
Soodarshan-Gajadhur <gajadhur@ualberta.ca>
2021-10-27 16:19:00 -0600

    added top toolbar in homescreen

--- homescreen/app/src/main/java/com/example/homescreen/MainActivity.java
+++ homescreen/app/src/main/java/com/example/homescreen/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.homescreen;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
@@ -9,8 +10,9 @@ import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
+import android.view.Menu;
 import android.view.View;
+
 
 import com.google.android.material.badge.BadgeDrawable;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -19,12 +21,19 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 public class MainActivity extends AppCompatActivity {
 
     public FloatingActionButton floatingActionButton;
+    private Toolbar topBar;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         floatingActionButton = findViewById(R.id.fab);
+
+        topBar = findViewById(R.id.top_app_bar);
+        setSupportActionBar(topBar);
+
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -41,10 +50,16 @@ public class MainActivity extends AppCompatActivity {
                 .findFragmentById(R.id.fragmentContainerView);
         NavController navController = navHostFragment.getNavController();
 
-        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+//        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.habits_fragment,
+                R.id.today_fragment, R.id.events_fragment, R.id.friends_fragment).build();
+
+//        NavigationUI.setupWithNavController(topBar, navController, appBarConfiguration);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
 
-
+        // set notification count on top of friends icon
         bottomNavigationView.setBackground(null);
         BadgeDrawable badge = bottomNavigationView.getOrCreateBadge(R.id.friends_fragment);
         badge.setVisible(true);
@@ -53,5 +68,12 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.top_toolbar, menu);
+        return true;
     }
 }
