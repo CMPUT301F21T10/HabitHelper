732ebfc3b99f39e8cc59790012ed7e63e45f5714
Soodarshan-Gajadhur <gajadhur@ualberta.ca>
2021-10-28 08:55:57 -0600

    Set up loginActivity as Launcher activity in Manifest file

--- code/app/src/main/java/com/example/habithelper/LoginActivity.java
+++ code/app/src/main/java/com/example/habithelper/LoginActivity.java
@@ -16,8 +16,6 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -28,9 +26,6 @@ import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
-
-import java.util.ArrayList;
-import java.util.HashMap;
 
 
 public class LoginActivity extends AppCompatActivity implements NewUserFragment.OnFragmentInteractionListener{
--- code/app/src/main/java/com/example/habithelper/MainActivity.java
+++ code/app/src/main/java/com/example/habithelper/MainActivity.java
@@ -6,15 +6,12 @@ import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
 import com.google.android.material.badge.BadgeDrawable;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -27,29 +24,29 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_homescreen);
 
-        Intent intent = getIntent();
-        loginIntent = new Intent(this, LoginActivity.class);
-        //If the user has not been logged in yet
-        if (!intent.hasExtra("currentUser")){
-            //Open up the login screen
-           startActivity(loginIntent);
-        }
-        //if a user has been logged in, access their information
-        else{
-            FirebaseUser user = (FirebaseUser) intent.getExtras().get("currentUser");
-            if (user != null) {
-                String email = user.getEmail();
-
-                // The user's ID, unique to the Firebase project. Do NOT use this value to
-                // authenticate with your backend server, if you have one. Use
-                // FirebaseUser.getIdToken() instead.
-                String uid = user.getUid();
-            }else{
-                
-            }
-        }
+//        Intent intent = getIntent();
+//        loginIntent = new Intent(this, LoginActivity.class);
+//        //If the user has not been logged in yet
+//        if (!intent.hasExtra("currentUser")){
+//            //Open up the login screen
+//           startActivity(loginIntent);
+//        }
+//        //if a user has been logged in, access their information
+//        else{
+//            FirebaseUser user = (FirebaseUser) intent.getExtras().get("currentUser");
+//            if (user != null) {
+//                String email = user.getEmail();
+//
+//                // The user's ID, unique to the Firebase project. Do NOT use this value to
+//                // authenticate with your backend server, if you have one. Use
+//                // FirebaseUser.getIdToken() instead.
+//                String uid = user.getUid();
+//            }else{
+//
+//            }
+//        }
 
         //Initialize the database
         db = FirebaseFirestore.getInstance();
