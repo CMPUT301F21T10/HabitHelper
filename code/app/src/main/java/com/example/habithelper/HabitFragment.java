623507a6081190fc7dc6f82d5503503ca7f330b9
Aqil <91294196+v6k@users.noreply.github.com>
2021-11-03 22:01:57 -0600

    Merging nearly done. Fixed create habits event and view habit.

--- code/app/src/main/java/com/example/habithelper/HabitFragment.java
+++ code/app/src/main/java/com/example/habithelper/HabitFragment.java
@@ -71,8 +71,8 @@ public class HabitFragment extends Fragment implements Serializable, habitsCusto
 //            mParam2 = getArguments().getString(ARG_PARAM2);
 //        }
 
-        HabitsList.add(new Habit("Exercise", "To improve cardio", "09/11/2021", true));
-        HabitsList.add(new Habit("Meditation", "To relax", "13/11/2021", true));
+//        HabitsList.add(new Habit("Exercise", "To improve cardio", "09/11/2021", true));
+//        HabitsList.add(new Habit("Meditation", "To relax", "13/11/2021", true));
 
     }
 
