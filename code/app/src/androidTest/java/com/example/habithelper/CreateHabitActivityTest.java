1cea3cb488c26719ad1fcbf9afd9a4aadf2fdad0
Soodarshan-Gajadhur <gajadhur@ualberta.ca>
2021-11-04 22:37:53 -0600

    Added intent testing for viewHabitActivity

--- code/app/src/androidTest/java/com/example/habithelper/CreateHabitActivityTest.java
+++ code/app/src/androidTest/java/com/example/habithelper/CreateHabitActivityTest.java
@@ -1,7 +1,11 @@
 package com.example.habithelper;
 
+import static org.junit.Assert.assertEquals;
+
 import android.app.Activity;
+import android.text.Editable;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
@@ -11,6 +15,14 @@ import com.robotium.solo.Solo;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+
+/*
+* Note: Assumes user already has the following habits before running tests  in the correct order:
+* Meditation
+* Running
+* Rugby
+*
+* */
 
 public class CreateHabitActivityTest {
 
@@ -42,7 +54,7 @@ public class CreateHabitActivityTest {
      * Tests that a user can successfully go to CreateHabitActivity and click on cancel
      */
     @Test
-    public void GoToCreateHabitActivity(){
+    public void GoToCreateHabitActivityAndCancel(){
         // Asserts that the current activity is the LoginActivity. Otherwise, show “Wrong Activity”
         solo.assertCurrentActivity("Wrong Activity", LoginActivity.class);
 
@@ -51,15 +63,110 @@ public class CreateHabitActivityTest {
 
         solo.clickOnButton("Login");
         solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
-        solo.clickOnView(solo.getView(R.id.habits_recycler_view));
+        solo.clickOnText("Meditation");
         solo.assertCurrentActivity("Wrong Activity", ViewHabitsActivity.class);
 
-        solo.clickOnActionBarItem(R.id.goBack);
+        solo.clickOnView(solo.getView(R.id.goBack));
 
         solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
-
     }
 
 
+    /**
+     * Tests that a user can successfully go to CreateHabitActivity and click on save to save edited data
+     * Assumes that we already have a Meditation habit
+     */
+    @Test
+    public void GoToCreateHabitActivityAndSave(){
+        // Asserts that the current activity is the LoginActivity. Otherwise, show “Wrong Activity”
+        // logging in
+        solo.assertCurrentActivity("Wrong Activity", LoginActivity.class);
+        solo.enterText((EditText) solo.getView(R.id.loginEditEmail), "fake2@fake.ca");
+        solo.enterText((EditText) solo.getView(R.id.loginEditPassword), "123456");
+        solo.clickOnButton("Login");
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+
+
+        // edit meditation habit and save
+        solo.clickOnText("Meditation");
+        solo.assertCurrentActivity("Wrong Activity", ViewHabitsActivity.class);
+
+        solo.clearEditText((EditText) solo.getView(R.id.editTextTitle));
+        solo.enterText((EditText) solo.getView(R.id.editTextTitle), "Jogging");
+
+        solo.clickOnView(solo.getView(R.id.editTextStartDate));
+        solo.setDatePicker(0,2021,2,15);
+        solo.clickOnText("OK");
+
+        solo.clearEditText((EditText) solo.getView(R.id.editTextReason));
+        solo.enterText((EditText) solo.getView(R.id.editTextReason), "To stay healthy");
+
+        solo.clickOnView(solo.getView(R.id.save));
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+
+
+        //Checking if changes has been saved
+        solo.clickOnText("Jogging");
+        solo.assertCurrentActivity("Wrong Activity", ViewHabitsActivity.class);
+        ViewHabitsActivity activity = (ViewHabitsActivity) solo.getCurrentActivity();
+
+        final EditText titleText = activity.editTextTitle;
+        String title = titleText.getText().toString();
+        assertEquals("Jogging", title);
+
+        final TextView dateText = activity.editTextStartDate;
+        String date = dateText.getText().toString();
+        assertEquals("2021-3-15", date);
+
+        final EditText reasonText = activity.editTextReason;
+        String reason = reasonText.getText().toString();
+        assertEquals("To stay healthy", reason);
+    }
+
+
+    /**
+     * Tests that a user can successfully go to CreateHabitActivity and click on delete to delete a habit
+     * Assumes that we already have a Rugby habit
+     */
+    @Test
+    public void GoToCreateHabitActivityAndDelete(){
+        // Asserts that the current activity is the LoginActivity. Otherwise, show “Wrong Activity”
+        // logging in
+        solo.assertCurrentActivity("Wrong Activity", LoginActivity.class);
+        solo.enterText((EditText) solo.getView(R.id.loginEditEmail), "fake2@fake.ca");
+        solo.enterText((EditText) solo.getView(R.id.loginEditPassword), "123456");
+        solo.clickOnButton("Login");
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+
+
+        // edit meditation habit and save
+        solo.clickOnText("Running");
+        solo.assertCurrentActivity("Wrong Activity", ViewHabitsActivity.class);
+
+        solo.clickOnView(solo.getView(R.id.delete));
+
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+
+        MainActivity activity = (MainActivity) solo.getCurrentActivity();
+    }
+
+    /**
+     * Checks if habit Running has been deleted by previous test
+     */
+    @Test
+    public void checkIfHabitDeleted() {
+
+        // Asserts that the current activity is the LoginActivity. Otherwise, show “Wrong Activity”
+        solo.assertCurrentActivity("Wrong Activity", LoginActivity.class);
+
+        solo.enterText((EditText) solo.getView(R.id.loginEditEmail), "fake2@fake.ca");
+        solo.enterText((EditText) solo.getView(R.id.loginEditPassword), "123456");
+
+        solo.clickOnButton("Login");
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+
+        assertEquals(false, solo.searchText("Running"));
+
+    }
 
 }
