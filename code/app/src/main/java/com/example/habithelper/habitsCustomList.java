ecf8cb0c99711e0180bda793065b4186eb6bbfed
Aqil <91294196+v6k@users.noreply.github.com>
2021-11-25 22:25:52 -0700

    Fixed habit creation/habit view bug

--- code/app/src/main/java/com/example/habithelper/habitsCustomList.java
+++ code/app/src/main/java/com/example/habithelper/habitsCustomList.java
@@ -81,7 +81,10 @@ public class habitsCustomList extends RecyclerView.Adapter<habitsCustomList.MyVi
         holder.habitTitle_textView.setText(habits_list.get(position).getTitle());
         holder.habitComment_textView.setText(habits_list.get(position).getReason());
         holder.habitDate_textView.setText(habits_list.get(position).getDateStarted());
-        int progress = (Integer.parseInt(habits_list.get(position).getNumHabitEvents())*100)/(habits_list.get(position).getTotalDays());
+        int progress;
+        if(habits_list.get(position).getTotalDays()==0){
+            progress = 0;
+        }else progress = (Integer.parseInt(habits_list.get(position).getNumHabitEvents())*100)/(habits_list.get(position).getTotalDays());
 
         holder.pb.setProgress(progress);
 
