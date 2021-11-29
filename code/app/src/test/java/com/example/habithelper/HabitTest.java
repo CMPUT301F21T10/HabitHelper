13755719efadd151d3c4207c899e528499096fb5
Aqil <91294196+v6k@users.noreply.github.com>
2021-11-28 18:57:38 -0700

    Added jUnit tests for habit

--- code/app/src/test/java/com/example/habithelper/HabitTest.java
+++ code/app/src/test/java/com/example/habithelper/HabitTest.java
@@ -1,19 +1,13 @@
 package com.example.habithelper;
-
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
-
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-
 import org.junit.Test;
-
-import java.util.ArrayList;
 import java.util.HashMap;
 
 public class HabitTest {
     private Habit mockHabit(){
-        return new Habit("title", "reason", "dateStarted", true);
+        return new Habit("title", "reason", "date", true, "1100001",0);
     }
 
     @Test
@@ -27,15 +21,17 @@ public class HabitTest {
         assertNotEquals(null, habitData.get("habit_reason"));
         assertNotEquals(null, habitData.get("habit_date"));
         assertNotEquals(null, habitData.get("publicStatus"));
+        assertNotEquals(null, habitData.get("days"));
+        assertNotEquals(null, habitData.get("numHabitEvents"));
 
         //Test that the data is in the right places
         assertEquals(newHabit.getTitle(), habitData.get("habit_title"));
         assertEquals(newHabit.getReason(), habitData.get("habit_reason"));
         assertEquals(newHabit.getDateStarted(), habitData.get("habit_date"));
         assertEquals(newHabit.getPublicStatus().toString(), habitData.get("publicStatus"));
+        assertEquals(newHabit.getHabitDays(), habitData.get("days"));
+        assertEquals(newHabit.getNumHabitEvents(), habitData.get("numHabitEvents"));
     }
-
-
 
     @Test
     public void testSetName(){
@@ -53,5 +49,27 @@ public class HabitTest {
 
         newHabit.setReason("Long long long reason that is way too long and I cannot believe it is this long");
         assertEquals(30, newHabit.getReason().length());
+    }
+
+
+    @Test
+    public void testSetDate(){
+        Habit newHabit = mockHabit();
+        newHabit.setDateStarted("2021-11-11");
+        assertEquals("2021-11-11", newHabit.getDateStarted().toString());
+    }
+
+    @Test
+    public void testSetPrivacyStatus(){
+        Habit newHabit = mockHabit();
+        newHabit.setPublicStatus(false);
+        assertFalse(newHabit.getPublicStatus());
+    }
+
+    @Test
+    public void testHabitDays(){
+        Habit newHabit = mockHabit();
+        newHabit.setHabitDays("1010101");
+        assertEquals("1010101", newHabit.getHabitDays());
     }
 }
